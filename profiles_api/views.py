a609bacdc0989fe3cc205aca14bfa8adc38fe3fe
KenanBolat <kenan23@gmail.com>
2022-05-10 09:39:55 +0300

    Add search user profile

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,10 +1,11 @@
-from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework import status
-from . import serializers
 from rest_framework import viewsets
-from . import models
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
+from . import serializers
+from . import models
 from . import permissions
 
 
@@ -88,3 +89,6 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile, )
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
+
